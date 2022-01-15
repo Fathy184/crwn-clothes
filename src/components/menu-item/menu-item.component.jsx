37edf75f4dab5592ba8fbae51d67e339ac446a00
fathy <fa1846045@gmail.com>
2022-01-15 20:39:12 +0200

    broken homepage into smaller components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP Now</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
